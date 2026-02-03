63e77d4e82b7b651bf4dfa06121ff3c192f9b78d
Mathias BERGER <mathias.codage@gmail.com>
2026-02-03 19:20:33 +0100

    🎨Réctification du Footer 🐍

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <img src={logoFooter} alt="Kasa" className="footer__logo" />
+      <p className="footer__text">© 2020 Kasa. All rights reserved</p>
     </footer>
   )
 }
