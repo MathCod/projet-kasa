af8917d62b95102f77bf5d811a1393516edcce8b
Mathias BERGER <mathias.codage@gmail.com>
2026-02-09 15:40:44 +0100

    ➕ mise en place de la page de logements & 🐍

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,9 +1,23 @@
+import { useParams, Navigate } from 'react-router-dom'
+import logements from "../data/logements.json"
+
+
 function Housing() {
+  const { id } = useParams()
+  const logement = logements.find((item) => item.id === id)
+
+  if (!logement) {
+    return <Navigate to="/error" />
+  }
+
   return (
-    <div>
-      <h1>Fiche Logements</h1>
+    <div className="housing">
+      <h1>{logement.title}</h1>
+      <p>{logement.location}</p>
+      
+      
     </div>
-  );
+  )
 }
 
-export default Housing;
+export default Housing
