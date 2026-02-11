e236e9f624918596379768cc2edf3059e21e0a98
Mathias BERGER <mathias.codage@gmail.com>
2026-02-11 10:51:04 +0100

    ➕🎨Police Montserrat & Variables

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
 function Error() {
   return (
-    <div>
-      <h1>404 - page non trouvée</h1>
+    <div className="error">
+      <h1 className="error__title">404</h1>
+      <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error__link">Retourner sur la page d'accueil</Link>
     </div>
-  );
+  )
 }
 
-export default Error;
+export default Error
