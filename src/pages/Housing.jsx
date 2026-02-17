47e649b79e5cc7bfb0ff1275943ae9023e970d80
Mathias BERGER <mathias.codage@gmail.com>
2026-02-17 19:45:06 +0100

    🎨Page des logements

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -13,41 +13,41 @@ function Housing() {
 
   const range = [1, 2, 3, 4, 5]
 
-  return (
+return (
     <div className="housing">
       <Slideshow pictures={logement.pictures} />
-      <div className="housing__title-container">
-        <h1>{logement.title}</h1>
-        <p>{logement.location}</p>
-        
-        <div className="housing__tags">
-          {logement.tags.map((tag, index) => (
-            <span key={index} className="tag">{tag}</span>
-          ))}
+      
+      <div className="housing__header">
+        <div className="housing__title-container">
+            <h1 className="housing__title">{logement.title}</h1>
+            <p className="housing__location">{logement.location}</p>
+          <div className="housing__tags">
+            {logement.tags.map((tag, index) => (
+              <span key={index} className="tag">{tag}</span>
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="housing__host-container">
-        <div className="housing__host">
-          <p>{logement.host.name}</p>
-          <img src={logement.host.picture} alt={logement.host.name} />
-        </div>
-        <div className="housing__rating">
-          {range.map((rangeElem) =>
-            logement.rating >= rangeElem ? (
-              <img key={rangeElem.toString()} src={redStar} alt="étoile rouge" />
-            ) : (
-              <img key={rangeElem.toString()} src={greyStar} alt="étoile grise" />
-            )
-          )}
+        <div className="housing__host-container">
+          <div className="housing__host">
+            <p>{logement.host.name}</p>
+            <img src={logement.host.picture} alt={logement.host.name} />
+          </div>
+          <div className="housing__rating">
+            {range.map((rangeElem) =>
+              logement.rating >= rangeElem ? (
+                <img key={rangeElem.toString()} src={redStar} alt="étoile rouge" />
+              ) : (
+                <img key={rangeElem.toString()} src={greyStar} alt="étoile grise" />
+              )
+            )}
+          </div>
         </div>
       </div>
 
       <div className="housing__details">
         <Collapse title="Description" content={logement.description} />
-        <Collapse 
-          title="Équipements" 
-          content={
+        <Collapse title="Équipements" content={
             <ul>
               {logement.equipments.map((eq, index) => (
                 <li key={index}>{eq}</li>
