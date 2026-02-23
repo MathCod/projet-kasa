6bf4dbb298e753b06c43082e8515047a669321eb
Mathias BERGER <mathias.codage@gmail.com>
2026-02-23 18:52:14 +0100

    🎨Dernières retouches scss

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header"
 
 function Error() {
   return (
-    <div>
+    <div className="error-wrapper">
       <Header />
       <div className="error">
         <h1 className="error__title">404</h1>
